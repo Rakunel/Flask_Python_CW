1a8b130dbfeae13b2f4829ca5d888666f59158f8
Burhan Sahin <rsahins18@gmail.com>
2024-03-11 22:44:19 +0200

    second push

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,26 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return "Hello World Clarusway"
 
+@app.route("/second")
+def head2():
+    return "Fenerin Maci Var"
+
+@app.route("/third")
+def head3():
+    return "Sampiyon Fener"
+
+@app.route("/forth/<string:id>")
+def head4(id):
+    return f'Fenerin total puani = {id}'
 
 
 
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    # app.run(debug=True, port=30000)
+    app.run(host= '0.0.0.0', port=8081)
